0c46c8f47c06bc577888fe4d19502f38806917cf
Bhaskar M <bhaskarmachcha2@gmail.com>
2019-04-28 19:26:20 +0530

    :tada: Adding greeting feature
    :arrow_up: Upgrading dependency

--- src/components/ng-greet.component.ts
+++ src/components/ng-greet.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'ng-greet',
@@ -7,5 +7,24 @@ import { Component } from '@angular/core';
 })
 
 export class NgGreetComponent {
-  
+
+  @Input('greetDate')
+  greetDate: Date;
+
+  public greetMessage: string;
+
+  ngOnInit() {
+    if (this.greetDate) {
+      console.log("greetDate=>", this.greetDate);
+      let grHours = this.greetDate.getHours();
+      if (grHours > 0 && grHours < 12) {
+        this.greetMessage = "Good morning";
+      } else if (grHours > 12 && grHours < 18) {
+        this.greetMessage = "Good afternoon";
+      } else {
+        this.greetMessage = "Good evening";
+      }
+    }
+  }
+
 }
